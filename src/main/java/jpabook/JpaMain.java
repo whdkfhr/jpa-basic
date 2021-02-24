00449db8abe166017e9645d6f1cc363d4f1b005e
whdkfhr <whwoals18@naver.com>
2021-02-24 14:17:15 +0900

    요구사항 분석 및 기본 매핑

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -1,25 +1,20 @@
-package hellojpa;
+package jpabook;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member member = new Member(200L, "D");
-            em.persist(member);
-            em.flush();
-
-            System.out.println("===============");
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
